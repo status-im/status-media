2941ca817d69d5b024ebc64bbc9d63795b39e4b8
jinhojang6 <jinho5051@gmail.com>
2020-01-28 23:01:39 +0900

    update URLs

--- src/data/podcasts.js
+++ src/data/podcasts.js
@@ -44,122 +44,58 @@ export const categories = [
 export const podcasts = [
   {
     category: 'general',
-    name: 'Syntax - Tasty Web Development Treats',
-    link: 'https://feed.syntax.fm/rss',
+    name: 'Everything Status',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/60/4e/d6/604ed6ba-6a5a-5faa-edf5-189a290f3aa3/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/W5mQLucviakmoHAFldUZbtFkTHw91vbcQk0w8o3woHEm9c4lpaMqi4cmY6bl3HmTSTnCxL5dvwwbDQa0eVGHrT16dhjEQmoAvg1Mk1Gjy-i3QtJjFRk1o5RxaWQ0BDfVn3oTntFF=w2400'
   },
   {
     category: 'status',
-    name: 'The freeCodeCamp Podcast',
-    link: 'http://freecodecamp.libsyn.com/rss',
+    name: 'Status News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/de/83/f9/de83f9d5-1745-8572-56d9-b10f62824807/source/100x100bb.jpg'
-  },
-  {
-    category: 'embark',
-    name: 'Full Stack Radio',
-    link: 'https://rss.simplecast.com/podcasts/279/rss',
-    img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/91/8f/bc/918fbcc5-9e95-4a42-c8f4-50f2321fcc9d/source/100x100bb.jpg'
-  },
-  {
-    category: 'nimbus',
-    name: 'Tech Jr',
-    link: 'https://techjr.dev/rss.xml',
-    img:
-      'https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/bc/ae/5d/bcae5d27-c7d9-6a89-a59f-103049db812b/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/cShjrh-GnRg5Sc-DRudnZ6isn_v89EeyTZeFZ3Liicpgu3MNYVk6rv8mNutI3038TqegBAUEO9jro4z5noVCuqeEWY-yi8WsdhdvQCvfAYM0251VhZw1fCUqMR6FI7lGwYdM2ALu=w2400'
   },
   {
     category: 'keycard',
-    name: 'Base.cs Podcast',
-    link: 'http://feeds.codenewbie.org/basecs_podcast.xml',
+    name: 'Keycard News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/11/79/e5/1179e5b5-3899-2c26-175f-d4ab664c5197/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/z6xrfsk2CcuftPWBaoCz7BLUbDYFdkAABR7J85540omByCRbozmalDhjl-GgMJiG6qQ6k6bPWsuRkeKq_nD2ZpWapnmJmSbueySV20loeNSduDVIthUYaaDkxaI8e6X89THnCFjs=w2400'
   },
   {
     category: 'assemble',
-    name: 'CodeNewbie',
-    link: 'http://feeds.codenewbie.org/cnpodcast.xml',
+    name: 'Assemble News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/64/02/75/640275f5-5c8e-27ba-352e-3e68449b1335/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/ziNreb7iaFUG8y2h8RqTIcsuVShPvV3c6fvRUxjHRr0ow4uXIbkEz_QMK7bdDHoMYqkwk4SykVgQA8bibZppHsPRnA76XO8zwCbpFxnYeEyiJE6P_udN1MensMHg7zsQQOhJl8X_=w2400'
   },
   {
     category: 'teller',
-    name: 'The Console Log',
-    link: 'https://theconsolelog.com/rss.xml',
+    name: 'Teller News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/43/a5/7d/43a57d0b-943b-1f77-1089-a460c9708e51/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/y2WgfFSPuWJO7JQSg0pMVGpu8G8jtb77dJ6zwI9duV1Vr3RI2ORKwaLFtVx_MuDdy8_J4bdTJpp7djqmhHatEQAE3DWKOZiUh4jqMCvDf_FvKpbXZX9FsOoRKS_vEpllxKhfxfoI=w2400'
   },
   {
     category: 'vac',
-    name: 'Developer Tea',
-    link: 'https://rss.simplecast.com/podcasts/363/rss',
+    name: 'Vac News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/60/32/c0/6032c0e8-be92-00ed-f693-1f4ff4205bf1/source/100x100bb.jpg'
-  },
-  {
-    category: 'general',
-    name: 'La Vie en Code',
-    link: 'http://lavieencode.libsyn.com/rss',
-    img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/06/ea/e5/06eae525-675c-d89d-8b21-b36e3d779f42/source/100x100bb.jpg'
-  },
-  {
-    category: 'general',
-    name: 'CS50 Podcast',
-    link: 'https://s3.amazonaws.com/cdn.cs50.net/podcast/podcast.rss',
-    img:
-      'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/60/a6/3e/60a63ea5-c5ba-f211-10ce-5c027d937204/source/100x100bb.jpg'
-  },
-  {
-    category: 'general',
-    name: 'Coding Blocks',
-    link: 'http://feeds.podtrac.com/tBPkjrcL0_m0',
-    img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/e6/f6/2c/e6f62c12-6b26-005f-53bd-c6ac91f504ee/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/5nqIqZ0syycQGt704Hf_8ZlNytDHk-qtPry9Nrx-KMfJ_sy2LXAqQ8HqIRe_LFFnE33iGyJh6v0jAYvVa2-SwwcG7cbz7nleovli75MxqZ93Uh5gE6K1XZpBAoHyDGL8Rx58PgD3=w2400'
   },
   {
     category: 'embark',
-    name: 'JS Party',
-    link: 'https://changelog.com/jsparty/feed',
+    name: 'Embark News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is2-ssl.mzstatic.com/image/thumb/Music123/v4/18/55/87/18558757-eaf3-e4e3-023a-031ae4c716fc/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/3Cr-BRhNn3uVjlSemxVYMkA8BSiJTod1Rxq97Z5pLTBDxAkTLuMQ-9w0f1fuV99SLH_Sr4DGw6VLmdx6RjWXKmiAg_laQdmSzWsDuSR9buv70xirfvGLVNHLK83wBrMsyUJqTnyU=w2400'
   },
   {
     category: 'nimbus',
-    name: 'React Podcast',
-    link: 'https://rss.simplecast.com/podcasts/6265/rss',
+    name: 'Nimbus News',
+    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
     img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/22/2b/d3/222bd331-5d17-28dd-7494-6cec6f7d07bd/source/100x100bb.jpg'
-  },
-  {
-    category: 'general',
-    name: 'The Changelog',
-    link: 'https://changelog.com/podcast/feed',
-    img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/bd/4a/62/bd4a6284-5316-0fd2-d8b4-5ddfc1214279/source/100x100bb.jpg'
-  },
-  {
-    category: 'status',
-    name: 'Front End Happy Hour',
-    link:
-      'https://podcasts.files.bbci.co.uk/p06kyljg.rss',
-    img:
-      'https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/b6/94/68/b69468bd-753d-50c5-f638-a7a920a0ab16/source/100x100bb.jpg'
-  },
-  {
-    category: 'status',
-    name: 'Learn to Code With Me',
-    link: 'http://learntocodewithme.libsyn.com/rss',
-    img:
-      'https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/52/55/13/52551353-8aea-915c-f00d-460c6a6ea764/source/100x100bb.jpg'
-  },
-  {
-    category: 'status',
-    name: 'Ladybug Podcast',
-    link: 'https://pinecast.com/feed/ladybug-podcast',
-    img:
-      'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/96/8f/50/968f5054-b35c-50f2-801a-3cc4f4d87707/source/100x100bb.jpg'
+      'https://lh3.googleusercontent.com/Udf471HbwpX53XWm6Hzu9stQj760xEHW_t2puUtpXbt3yTCwvlaSVcWGQPc21DSpVmFdIm9J7YT2LE7UnpZGLxyQ8wAbedYs9TYpwJ8NYYp0CZeBgaur6i0lKuTp-rV9lmEMHuwc=w2400'
   }
 ]
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const input = `
+## This is a header
+<br>
+# And this is a paragraph
+
+hi there
+`
 
 class Upload extends Component {
   render() {
     return (
-      <div className='middle'>
-        <h1>Coming soon!</h1>
+      <div style={{ textAlign: 'left'}}>
+        <ReactMarkdown 
+          source={input}
+          escapeHtml={false}
+        />
       </div>
     )
   }
