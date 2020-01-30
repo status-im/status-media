81c5395639aefc1c18331457f2cd79b35035fe42
jinhojang6 <jinho5051@gmail.com>
2020-01-31 00:02:35 +0900

    add a couple of podcast channels

--- src/data/podcasts.js
+++ src/data/podcasts.js
@@ -50,51 +50,65 @@ export const podcasts = [
       'https://lh3.googleusercontent.com/W5mQLucviakmoHAFldUZbtFkTHw91vbcQk0w8o3woHEm9c4lpaMqi4cmY6bl3HmTSTnCxL5dvwwbDQa0eVGHrT16dhjEQmoAvg1Mk1Gjy-i3QtJjFRk1o5RxaWQ0BDfVn3oTntFF=w2400'
   },
   {
+    category: 'general',
+    name: 'The Bitcoin Podcast',
+    link: 'https://feeds.simplecast.com/xCQr3ykc',
+    img:
+      'https://lh3.googleusercontent.com/h-NtM2BbbFddHMNyTGBmGdzge-zqyy9-LCSUnRF8r7st1s6ZIzZM01YFTAcFkvlf8TfF2cptTBSlNUGEVpf_1SM8LPO00tPo6OAKaSDePLu6_TC6IBWSZpVeIr1aTvDXrEwuFqPQ0w=w2400'
+  },
+  {
+    category: 'general',
+    name: 'Epicenter',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
+    img:
+      'https://lh3.googleusercontent.com/cwsJClMXMYPuo17tQmeZDYhXI7ZKQta4AUGJ0mKrFaPBpRFssVpxLvzE-eScQ8vdBkTvcB9B5oF_tyY_gQAix-7muSwBfPrj1x5Pfa4r-6TCXJqxg6l8beQ9GVGjlDquy0I_oYlGKg=w2400'
+  },
+  {
     category: 'status',
     name: 'Status News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/cShjrh-GnRg5Sc-DRudnZ6isn_v89EeyTZeFZ3Liicpgu3MNYVk6rv8mNutI3038TqegBAUEO9jro4z5noVCuqeEWY-yi8WsdhdvQCvfAYM0251VhZw1fCUqMR6FI7lGwYdM2ALu=w2400'
   },
   {
     category: 'keycard',
     name: 'Keycard News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/z6xrfsk2CcuftPWBaoCz7BLUbDYFdkAABR7J85540omByCRbozmalDhjl-GgMJiG6qQ6k6bPWsuRkeKq_nD2ZpWapnmJmSbueySV20loeNSduDVIthUYaaDkxaI8e6X89THnCFjs=w2400'
   },
   {
     category: 'assemble',
     name: 'Assemble News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/ziNreb7iaFUG8y2h8RqTIcsuVShPvV3c6fvRUxjHRr0ow4uXIbkEz_QMK7bdDHoMYqkwk4SykVgQA8bibZppHsPRnA76XO8zwCbpFxnYeEyiJE6P_udN1MensMHg7zsQQOhJl8X_=w2400'
   },
   {
     category: 'teller',
     name: 'Teller News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/y2WgfFSPuWJO7JQSg0pMVGpu8G8jtb77dJ6zwI9duV1Vr3RI2ORKwaLFtVx_MuDdy8_J4bdTJpp7djqmhHatEQAE3DWKOZiUh4jqMCvDf_FvKpbXZX9FsOoRKS_vEpllxKhfxfoI=w2400'
   },
   {
     category: 'vac',
     name: 'Vac News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/5nqIqZ0syycQGt704Hf_8ZlNytDHk-qtPry9Nrx-KMfJ_sy2LXAqQ8HqIRe_LFFnE33iGyJh6v0jAYvVa2-SwwcG7cbz7nleovli75MxqZ93Uh5gE6K1XZpBAoHyDGL8Rx58PgD3=w2400'
   },
   {
     category: 'embark',
     name: 'Embark News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/3Cr-BRhNn3uVjlSemxVYMkA8BSiJTod1Rxq97Z5pLTBDxAkTLuMQ-9w0f1fuV99SLH_Sr4DGw6VLmdx6RjWXKmiAg_laQdmSzWsDuSR9buv70xirfvGLVNHLK83wBrMsyUJqTnyU=w2400'
   },
   {
     category: 'nimbus',
     name: 'Nimbus News',
-    link: 'http://feeds.soundcloud.com/users/soundcloud:users:741837922/sounds.rss',
+    link: 'https://feeds.simplecast.com/lKmQDG9R',
     img:
       'https://lh3.googleusercontent.com/Udf471HbwpX53XWm6Hzu9stQj760xEHW_t2puUtpXbt3yTCwvlaSVcWGQPc21DSpVmFdIm9J7YT2LE7UnpZGLxyQ8wAbedYs9TYpwJ8NYYp0CZeBgaur6i0lKuTp-rV9lmEMHuwc=w2400'
   }
